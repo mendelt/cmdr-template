dd4ed9366b7f51e67bfddf472181b78c2110d3bd
Mendelt Siebenga <msiebenga@gmail.com>
2019-04-14 19:34:04 +0200

    Use simple cmdr example project as base for the template

---
+++ README.md
@@ -0,0 +1,10 @@
+# bluepill-template
+Cargo generate template for cmdr projects.
+
+## Usage
+Install cargo-generate if you don't already have it;
+```cargo install cargo-generate```
+
+Generate your project;
+```cargo generate --git https://github.com/mendelt/cmdr-template```
+
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
+//! GreeterScope implements two commands, one greets the user with the supplied name. The other
+//! returns CommandResult::Quit to quit the application.
+
+use cmdr::*;
+
+struct GreeterScope {}
+
+/// Example scope that implements two commands, greet and quit
+#[cmdr]
+impl GreeterScope {
+    /// Cmdr command to greet someone.
+    /// Takes one parameter and prints a greeting
+    #[cmd(greet)]
+    fn greet_method(&self, args: &[String]) -> CommandResult {
+        println!("Hello {}", args[0]);
+        CommandResult::Ok
+    }
+
+    /// Cmdr command to quit the application by returning CommandResult::Quit
+    #[cmd(quit, help = "Quit the application", alias(exit, x, q))]
+    fn quit_method(&self, _args: &[String]) -> CommandResult {
+        println!("Quitting");
+        CommandResult::Quit
+    }
+}
+
+/// Main function that creates the scope and starts a command loop for it
 fn main() {
-    println!("Hello, world!");
+    cmd_loop(&mut GreeterScope {});
 }
